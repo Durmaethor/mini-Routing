cb296af871405fd9e6fd0d45e78500e4fed5dc5c
Ryan Kramer <rkramer613@gmail.com>
2017-02-21 19:02:10 -0700

    Stating the states

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 angular.module('miniRouting', [ui.router]);
+
+.config(function($urlRouterProvider, $stateProvider){
+
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'js/home/homeTmpl.html',
+            controller: 'homeCtrl'
+        })
+        
+        .state('settings', {
+            url: '/settings',
+            templateUrl: 'js/settings/settingsTmpl.html',
+        })
+
+        .state('products', {
+            url: 'js/products/:id',
+            templateUrl: 'js/products/productTmpl.html',
+            controller: 'productsCtrl'
+        })
+    
+   
+});
